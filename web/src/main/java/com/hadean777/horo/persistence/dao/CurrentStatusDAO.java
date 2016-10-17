ad87f6c30d77927b9eac3f1039da7d36f6b55157
sergey <hadean777@gmail.com>
2016-10-17 19:07:17 +0300

    DAO layer updated

--- web/src/main/java/com/hadean777/horo/persistence/dao/CurrentStatusDAO.java
+++ web/src/main/java/com/hadean777/horo/persistence/dao/CurrentStatusDAO.java
@@ -1,8 +1,17 @@
 package com.hadean777.horo.persistence.dao;
 
+import java.util.List;
+
+import com.hadean777.horo.exception.DAOException;
 import com.hadean777.horo.persistence.pojo.CurrentStatus;
+
 
 public interface CurrentStatusDAO extends GenericDAO<CurrentStatus, Long> {
 	
-
+	/**
+	 * Returns list of current statuses <br>
+	 * 
+	 * @return {@link List<Sign>}
+	 */
+    public List<CurrentStatus> getCurrentStatuses() throws DAOException;
 }
--- web/src/main/java/com/hadean777/horo/persistence/dao/hibernate/CurrentStatusDAOHibernateImpl.java
+++ web/src/main/java/com/hadean777/horo/persistence/dao/hibernate/CurrentStatusDAOHibernateImpl.java
@@ -1,9 +1,14 @@
 package com.hadean777.horo.persistence.dao.hibernate;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
+import com.hadean777.horo.exception.DAOException;
 import com.hadean777.horo.persistence.dao.CurrentStatusDAO;
 import com.hadean777.horo.persistence.pojo.CurrentStatus;
 
@@ -11,6 +16,19 @@ import com.hadean777.horo.persistence.pojo.CurrentStatus;
 @Transactional
 public class CurrentStatusDAOHibernateImpl extends GenericDAOHibernateImpl<CurrentStatus, Long> 
 implements CurrentStatusDAO {
+
+	@SuppressWarnings("unchecked")
+	public List<CurrentStatus> getCurrentStatuses() throws DAOException{
+		
+        List<CurrentStatus> currentStatusList = null;
+        try {
+            Query query = getSession().createQuery("from CurrentStatus");
+            currentStatusList = (List<CurrentStatus>) query.list();
+        } catch (HibernateException e) {
+            throw new DAOException(e);
+        }
+        return currentStatusList;
+	}
 
 
 }
