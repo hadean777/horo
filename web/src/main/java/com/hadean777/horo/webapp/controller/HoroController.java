88179ba53a282a64131e6b563bbfd452d09f58f7
sergey <hadean777@gmail.com>
2016-11-01 16:30:13 +0200

    Controller changed to RestController

--- web/src/main/java/com/hadean777/horo/webapp/controller/HoroController.java
+++ web/src/main/java/com/hadean777/horo/webapp/controller/HoroController.java
@@ -1,6 +1,6 @@
 package com.hadean777.horo.webapp.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,8 +9,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hadean777.horo.model.ShowDisplayStatus;
 
-@Controller
-public class RestController {
+@RestController
+public class HoroController {
 
 	@RequestMapping(value = "/common/getHoro.do", method = RequestMethod.POST)
 	@ResponseBody
